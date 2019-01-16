dd770db382326e4d310b9cccdfe04f8e49ade20d
Roy Berkowitz <roy@storemaven.com>
2019-01-17 01:34:51 +0200

    form validation + submit flow

--- lemonoa/src/app/schedule-form/schedule-form.component.ts
+++ lemonoa/src/app/schedule-form/schedule-form.component.ts
@@ -9,11 +9,12 @@ import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@ang
 export class ScheduleFormComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
+  JSON = JSON;
   sessions = [
-    { id: 100, name: 'Tuesday 18:00' },
-    { id: 200, name: 'Tuesday 19:00' },
-    { id: 300, name: 'Wednesday: 17:00' },
-    { id: 400, name: 'Thursday 17:00' }
+    { id: 354252454, name: 'Tuesday 18:00' },
+    { id: 425245245, name: 'Tuesday 19:00' },
+    { id: 134134134, name: 'Wednesday: 17:00' },
+    { id: 234242344, name: 'Thursday 17:00' }
   ];
 
 
@@ -22,9 +23,9 @@ export class ScheduleFormComponent implements OnInit {
 
   ngOnInit() {
     const sessions = this.sessions.map(s => new FormControl(false));
-    sessions[0].setValue(true); // Set the first checkbox to true (checked)
+    // sessions[0].setValue(true); // Set the first checkbox to true (checked)
       this.registerForm = this.formBuilder.group({
-          fullName: ['', Validators.required, Validators.minLength(2)],
+          fullName: ['', [Validators.required, Validators.minLength(2)]],
           email: ['', [Validators.required, Validators.email, Validators.minLength(5)]],
           phone: ['', [Validators.required, Validators.minLength(10)]],
           availableSessions: new FormArray(sessions)
@@ -34,5 +35,13 @@ export class ScheduleFormComponent implements OnInit {
   get f() { return this.registerForm.controls; }
 
   onSubmit() {
-      this.submitted = true;
+    this.submitted = true;
+      const sessionScheduled = this.registerForm.value.availableSessions
+      .map((v, i) => v ? this.sessions[i].id : null)
+      .filter(v => v !== null);
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      console.log(sessionScheduled, this.f['email'].value, this.f['fullName'].value, this.f['phone'].value);
+    }
 }}
